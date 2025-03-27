9a75c1a69d0d55c9b869678c9d3dc52c595398b9
Patrick Trumpis <patrick.trumpis@gmx.de>
2025-03-28 00:56:39 +0100

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import webpack from 'webpack';
+import TerserPlugin from 'terser-webpack-plugin';
+import pkg from './package.json' with { type: 'json' };
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const config = {
+  mode: 'production',
+  plugins: [
+    new webpack.DefinePlugin({
+      __VERSION__: JSON.stringify(pkg.version),
+    }),
+  ],
+  optimization: {
+    usedExports: false,
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          keep_classnames: true,
+          keep_fnames: true,
+        },
+      }),
+    ],
+  },
+}
+
+const configWeb = {
+  ...config,
+  entry: './src/web/index.js',
+  output: {
+    filename: 'web.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
+
+export default [configWeb];
