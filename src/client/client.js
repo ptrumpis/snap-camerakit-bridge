04978624916cc48b8c34f73391d85395dd775a08
Patrick Trumpis <patrick.trumpis@gmx.de>
2025-03-28 00:58:25 +0100

    Add camera kit client

---
+++ src/client/client.js
@@ -0,0 +1,83 @@
+import WebSocket from 'ws';
+import { BridgeError, MessageError } from '../common/errors.js';
+import { Message, CallMessage } from '../common/messages.js';
+import pkg from '../../package.json' with { type: 'json' };
+
+class CameraKitClient {
+    #address;
+    #timeout;
+
+    constructor(address, options = {}) {
+        this.#address = address;
+        this.#timeout = options.timeout || 6000;
+    }
+
+    async init(apiToken) {
+        const message = new CallMessage('init', [apiToken]);
+        return this.#sendMessage(message);
+    }
+
+    async getLensGroup(groupId, withMeta = true) {
+        const message = new CallMessage('getLensGroup', [groupId, withMeta]);
+        return this.#sendMessage(message);
+    }
+
+    async getLensMeta(lensId) {
+        const message = new CallMessage('getLensMeta', [lensId]);
+        return this.#sendMessage(message);
+    }
+
+    async #sendMessage(message) {
+        const socket = new WebSocket(this.#address, `CameraKitClient-${pkg.version}`);
+
+        const timeoutId = setTimeout(() => {
+            socket.terminate();
+        }, this.#timeout);
+
+        return new Promise((resolve, reject) => {
+            socket.on('open', () => {
+                clearTimeout(timeoutId);
+
+                socket.on('message', (data) => {
+                    let message = null;
+                    try {
+                        let dataString = data instanceof Buffer ? data.toString() : data;
+                        if (typeof dataString !== 'string') {
+                            throw new Error(`Received invalid data type: ${typeof dataString}`);
+                        }
+                        message = Message.fromJSON(JSON.parse(dataString));
+                        resolve(message);
+                    } catch (e) {
+                        reject(new MessageError(e.message, e.name));
+                    } finally {
+                        socket.close();
+                    }
+                });
+
+                socket.send(JSON.stringify(message));
+            });
+
+            socket.on('close', (code) => {
+                reject(new BridgeError(`Connection closed. Code: ${code}`));
+            });
+
+            socket.on('error', (err) => {
+                reject(new BridgeError(err.message, err.name));
+            });
+        }).finally(() => {
+            clearTimeout(timeoutId);
+
+            return new Promise((resolve) => {
+                if (socket.readyState === WebSocket.CLOSED) {
+                    resolve();
+                } else {
+                    socket.on('close', resolve);
+                    socket.close();
+                }
+            });
+        });
+    }
+}
+
+export { CameraKitClient };
+export default CameraKitClient;
