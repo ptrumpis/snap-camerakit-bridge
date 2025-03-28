939c210072c5f87a35b400c2f97c376389491582
Patrick Trumpis <patrick.trumpis@gmx.de>
2025-03-28 02:19:29 +0100

    Add index.js and exports

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import CameraKitBridge from '../src/bridge/bridge.js';
+import CameraKitClient from '../src/client/client.js';
+import CameraKitServer from '../src/server/server.js';
+import * as Errors from '../src/common/errors.js';
+import * as Messages from '../src/common/messages.js';
+
+export * from '../src/bridge/bridge.js';
+export * from '../src/client/client.js';
+export * from '../src/server/server.js';
+export * from '../src/common/errors.js';
+export * from '../src/common/messages.js';
+
+export default { CameraKitBridge, CameraKitClient, CameraKitServer, ...Errors, ...Messages };
