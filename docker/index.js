4a89b85cdc273ca112eca91c1e51d94441e9e53e
Patrick Trumpis <patrick.trumpis@gmx.de>
2025-04-01 22:55:40 +0200

    Show friendly error on failure

--- docker/index.js
+++ docker/index.js
@@ -6,8 +6,12 @@ import { CameraKitBridge, CameraKitServer } from "../src/index.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const server = new CameraKitServer({ httpPort: 8080, socketPort: 3000, rootDir: join(__dirname, '../dist') })
-await server.start();
+try {
+    const server = new CameraKitServer({ httpPort: 8080, socketPort: 3000, rootDir: join(__dirname, '../dist') })
+    await server.start();
 
-const bridge = new CameraKitBridge({ targetUrl: 'https://localhost:8080/?websocketPort=3000'});
-await bridge.start();
+    const bridge = new CameraKitBridge({ targetUrl: 'https://localhost:8080/?websocketPort=3000' });
+    await bridge.start();
+} catch (e) {
+    console.error(e.message);
+}
