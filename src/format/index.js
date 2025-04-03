97507c05bd9fb7365e6c6f51f82ce5a47b98b50f
Patrick Trumpis <patrick.trumpis@gmx.de>
2025-04-04 01:29:04 +0200

    Update formatter exports

---
+++ src/format/index.js
@@ -0,0 +1,9 @@
+import LensFormatter from './LensFormatter.js';
+import OriginalFormatter from './OriginalFormatter.js';
+import SnapCameraFormatter from './SnapCameraFormatter.js';
+
+export * from './LensFormatter.js';
+export * from './OriginalFormatter.js';
+export * from './SnapCameraFormatter.js';
+
+export default { LensFormatter, OriginalFormatter, SnapCameraFormatter };
--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import CameraKitBridge from './bridge/CameraKitBridge.js';
 import CameraKitClient from './client/CameraKitClient.js';
 import CameraKitServer from './server/CameraKitServer.js';
 
-import LensFormatter from './format/LensFormatter.js';
-import OriginalFormatter from './format/OriginalFormatter.js';
-import SnapCameraFormatter from './format/SnapCameraFormatter.js';
-
+import * as Formatters from './format/index.js';
 import * as Actions from './common/actions.js';
 import * as Errors from './common/errors.js';
 import * as Messages from './common/messages.js';
@@ -14,12 +11,9 @@ export * from './bridge/CameraKitBridge.js';
 export * from './client/CameraKitClient.js';
 export * from './server/CameraKitServer.js';
 
-export * from './format/LensFormatter.js';
-export * from './format/OriginalFormatter.js';
-export * from './format/SnapCameraFormatter.js';
-
+export * from './format/index.js';
 export * from './common/actions.js';
 export * from './common/errors.js';
 export * from './common/messages.js';
 
-export default { CameraKitBridge, CameraKitClient, CameraKitServer, LensFormatter, OriginalFormatter, SnapCameraFormatter, ...Actions, ...Errors, ...Messages };
+export default { CameraKitBridge, CameraKitClient, CameraKitServer, ...Formatters, ...Actions, ...Errors, ...Messages };
